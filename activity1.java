9be75caaf3894eaea8e28218654c7a3ce3bb080d
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-02-21 18:50:19 +0800

    latest copy

--- activity1.java
+++ activity1.java
@@ -1,7 +1,9 @@
 public class activity1 {
     public static void main(String[] args) {
         System.out.println("List of Students:");
-
+        // Collaborators: Add your names below using the format:
+        // System.out.println("Your Full Name - Section");
+   
             System.out.println("MARC JOSH MARAVILLA - BSIT-2J");
     }
 }
