36f559af8e5f9cfa26937a07f4d71ef0116a0205
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-02-21 18:03:33 +0800

    THE ORIGINAL FILE

---
+++ activity1.java
@@ -0,0 +1,11 @@
+public class activity1 {
+    public static void main(String[] args) {
+        System.out.println("List of Students:");
+
+        // Collaborators: Add your names below using the format:
+        // System.out.println("Your Full Name - Section");
+        //EXAMPLE:
+        System.out.println("JUAN DELA CRUZ - BSIT-2J");
+    }
+}
+  
