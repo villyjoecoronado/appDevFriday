9e4383e089f2e47463b22cbd4a119e2c1f8a667a
sirjun <juntabios75@gmail.com>
2025-02-21 20:54:16 +0800

    Delete dibulo's file

--- Prog1.java
+++
@@ -1,8 +0,0 @@
-public class Prog1 {
-public static void main(strings{} args) {
-	System.out.println("PATEROS TECHNOLOGICAL COLLEGE")
-	System.out.println("PATEROS")
-	System.out.println("TECHNOLOGICAL")
-	System.out.println("BSIT 2L")
-	}
-}
